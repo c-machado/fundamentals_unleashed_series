c5b3900c02fe2b76081ef8cea0847315af4826f4
Carolina Machado <carolinamachado@Agustins-MacBook-Pro.local>
2023-09-25 19:28:42 +0200

    code clean up

--- ArraySequences/sentence_reversal.py
+++
@@ -1,5 +0,0 @@
-def sentence_reversal(phrase):
-    reverse = ' '.join(reversed(phrase.split(' ')))
-    print(reverse)
-
-sentence_reversal('this is an awesome example    ')
--- tests/AnagramTest.py
+++
@@ -1,9 +0,0 @@
-from nose.tools import assert_equal
-from ArraySequences import Anagram
-
-class AnagramTest(object):
-    def test(self, sol):
-        assert_equal(sol('go go go', 'gggooo'), True)
-
-t = Anagram()
-t.is_an_anagram(" og og go", "gggooo")
